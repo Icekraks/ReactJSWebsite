5354e5dbfdca4dd77442dafdde4a5d08c83e6054
Icekraks <stjimmy149@live.com.au>
2020-02-05 13:44:03 +1100

    Added youtube Resize CSS

--- src/App.js
+++ src/App.js
@@ -6,23 +6,24 @@ import {Button} from './Button';
 import {Sidebar} from './Sidebar';
 
 const items = [
-    {Buttonlabel: 'Home',id:'navButton',link: null},
-    {Buttonlabel: 'Youtube',id:'navButton',link: 'https://www.youtube.com/c/IcekraksOfficial'},
-    {Buttonlabel: 'Instagram',id:'navButton',link: 'https://www.instagram.com/icekraks/'},
-    {Buttonlabel: 'Twitter',id:'navButton',link:'https://twitter.com/OfficialIcekrak'},
-    {Buttonlabel: 'Github',id:'navButton',link:'https://github.com/Icekraks'}
+    {Buttonlabel: 'Home', id: 'navButton', link: null},
+    {Buttonlabel: 'Youtube', id: 'navButton', link: 'https://www.youtube.com/c/IcekraksOfficial'},
+    {Buttonlabel: 'Instagram', id: 'navButton', link: 'https://www.instagram.com/icekraks/'},
+    {Buttonlabel: 'Twitter', id: 'navButton', link: 'https://twitter.com/OfficialIcekrak'},
+    {Buttonlabel: 'Github', id: 'navButton', link: 'https://github.com/Icekraks'}
 ];
 const itemsM = [
-    {Buttonlabel: 'Home',id:'navButtonM',link: null},
-    {Buttonlabel: 'Youtube',id:'navButtonM',link: 'https://www.youtube.com/c/IcekraksOfficial'},
-    {Buttonlabel: 'Instagram',id:'navButtonM',link: 'https://www.instagram.com/icekraks/'},
-    {Buttonlabel: 'Twitter',id:'navButtonM',link:'https://twitter.com/OfficialIcekrak'},
-    {Buttonlabel: 'Github',id:'navButtonM',link:'https://github.com/Icekraks'}
+    {Buttonlabel: 'Home', id: 'navButtonM', link: null},
+    {Buttonlabel: 'Youtube', id: 'navButtonM', link: 'https://www.youtube.com/c/IcekraksOfficial'},
+    {Buttonlabel: 'Instagram', id: 'navButtonM', link: 'https://www.instagram.com/icekraks/'},
+    {Buttonlabel: 'Twitter', id: 'navButtonM', link: 'https://twitter.com/OfficialIcekrak'},
+    {Buttonlabel: 'Github', id: 'navButtonM', link: 'https://github.com/Icekraks'}
 ];
+
 //body, Everything below the header/banner is contained in the body class.
 export class App extends React.Component {
 
-    constructor(){
+    constructor() {
         super();
 
         this.state = {
@@ -30,8 +31,8 @@ export class App extends React.Component {
         };
     }
 
-    toggleNavBar = () =>{
-        this.setState( {
+    toggleNavBar = () => {
+        this.setState({
             navBarVisible: !this.state.navBarVisible
 
         });
@@ -48,16 +49,14 @@ export class App extends React.Component {
                 </div>
 
                 <div className="body">
-
-                    <div className='nav-bar'>
-                        <Sidebar className='sidebar-Buttons' items={items}/>
-                    </div>
-                    <div className='nav-bar-mobile'>
-                        <img src={logo2} className="App-logo2" alt="logo" onClick={this.toggleNavBar}/>
-                        {!this.state.navBarVisible && <Sidebar className='sidebar-Buttons-mobile' items={itemsM}/>}
-                    </div>
+                        <div className='nav-bar'>
+                            <Sidebar className='sidebar-Buttons' items={items}/>
+                        </div>
+                        <div className='nav-bar-mobile'>
+                            <img src={logo2} className="App-logo2" alt="logo" onClick={this.toggleNavBar}/>
+                            {!this.state.navBarVisible && <Sidebar className='sidebar-Buttons-mobile' items={itemsM}/>}
+                        </div>
                     <div className="columnBody">
-
                         <div className="rowBody">
                             <Button id="RSButton" label="Play Fucking Runescape."
                                     link="https://oldschool.runescape.com/"/>
@@ -65,8 +64,8 @@ export class App extends React.Component {
                         <div className="rowBody">
                             <Button id="RSButton" label="Click for Meme." link="http://www.jsfuck.com/"/>
                         </div>
-                        <div className="rowBody">
-                            <iframe title="Videos" width="560" height="315" src="https://www.youtube.com/embed/h9y2FXOcZl0"
+                        <div className="rowBody" >
+                            <iframe title="video" className="myVideo" src="https://www.youtube-nocookie.com/embed/h9y2FXOcZl0"
                                     frameBorder="0"
                                     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                                     allowFullScreen>
